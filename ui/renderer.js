08dbb376b5b6a114ba710b0b079db796ca0a6c0c
Jeffry Gaston <gastoj3@gmail.com>
2024-08-06 21:37:40 +0800

    preliminary renderer now putting some information onscreen

--- ui/renderer.js
+++ ui/renderer.js
@@ -1,4 +1,3 @@
-nodesByName = {}
 function setupInterface() {
   console.log("setting up interface")
   console.log("data:")
@@ -6,15 +5,46 @@ function setupInterface() {
 
   analyzeGraph()
 
-  goToNode("Using the terminal efficiently")
+  goToNode("What is a terminal?")
   console.log("done setting up interface")
 }
 
 function analyzeGraph() {
+  groupNodesByName()
+  findDependents()
+}
+
+function groupNodesByName() {
+  nodesByName = {}
   for (i = 0; i < knowledgeGraph.length; i++) {
     node = knowledgeGraph[i]
-    nodesByName[node["name"]] = node
+    name = node["name"]
+    nodesByName[name] = node
   }
+}
+
+function findDependents() {
+  nodeDependents = {}
+  // allocate map of dependents
+  for (i = 0; i < knowledgeGraph.length; i++) {
+    node = knowledgeGraph[i]
+    name = node["name"]
+    nodeDependents[name] = []
+  }
+  // populate map of dependents
+  for (i = 0; i < knowledgeGraph.length; i++) {
+    node = knowledgeGraph[i]
+    name = node["name"]
+    for (j = 0; j < node.dependencies.length; j++) {
+      dependencyName = node.dependencies[j]
+      console.log("dependency of " + name + " is " + dependencyName)
+      nodeDependents[dependencyName].push(name)
+    }
+  }
+}
+
+function getDependentNames(nodeName) {
+  return nodeDependents[nodeName]
 }
 
 function getNodeByName(name) {
@@ -27,6 +57,23 @@ function goToNode(nodeName) {
   name = node["name"]
   description = node["description"]
   dependencies = node["dependencies"]
-  render = name + "\n" + description + "\n" + dependencies
+  dependents = getDependentNames(nodeName)
+  render = "<h1>" + name + "</h1>" +
+           "<div>" + description + "</div>"
+  if (dependencies.length > 0) {
+    render += "<h2>Confused?</h2>"
+    for (i = 0; i < dependencies.length; i++) {
+      dependency = dependencies[i]
+      render += "<div>" + dependency + "</div>"
+    }
+  }
+  if (dependents.length > 0) {
+    render += "<h2>Curious?</h2>"
+    for (i = 0; i < dependents.length; i++) {
+      dependent = dependents[i]
+      render += "<div>" + dependent + "</div>"
+    }
+  }
+
   document.getElementById("content").innerHTML = "<div>" + render + "</div>"
 }
